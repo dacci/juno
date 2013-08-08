c324b14453c5f8f8e49836a822929e4058d0af15
Shinya Tsuda <wish.upon.a.planet@gmail.com>
2013-08-08 15:25:51 +0900

    Created precompiled header.

---
+++ juno/build/precompile.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2013 dacci.org
+
+#ifndef JUNO_BUILD_PRECOMPILE_H_
+#define JUNO_BUILD_PRECOMPILE_H_
+
+#include <assert.h>
+#include <crtdbg.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <atlbase.h>
+#include <atlstr.h>
+#include <atlutil.h>
+
+#include <atlapp.h>
+#include <atlcrack.h>
+#include <atlctrls.h>
+#include <atlddx.h>
+#include <atldlgs.h>
+#include <atlframe.h>
+
+#include <ws2tcpip.h>
+
+#include <list>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+#endif  // JUNO_BUILD_PRECOMPILE_H_
