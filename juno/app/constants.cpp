3d69c71632bc4abed94acced462047bc602c10d8
Shinya Tsuda <wish.upon.a.planet@gmail.com>
2016-02-13 11:47:41 +0900

    Changed application initialization.

---
+++ juno/app/constants.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2016 dacci.org
+
+#include <initguid.h>
+
+#include "app/constants.h"
--- juno/app/juno.cpp
+++ juno/app/juno.cpp
@@ -3,13 +3,8 @@
 #include "app/juno.h"
 
 #include <crtdbg.h>
-#include <stdint.h>
 
-#include <atlstr.h>
-
-#include <initguid.h>
-
-#include <madoka/net/winsock.h>
+#include <winsock2.h>
 
 #include <base/at_exit.h>
 #include <base/command_line.h>
@@ -19,89 +14,122 @@
 #include <url/url_util.h>
 
 #include "app/constants.h"
+#include "res/resource.h"
 #include "ui/main_frame.h"
 
 CAppModule _Module;
 
-int __stdcall wWinMain(HINSTANCE hInstance, HINSTANCE, wchar_t*, int) {
-#ifdef _DEBUG
-  {
-    int flags = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
-    flags |= _CRTDBG_LEAK_CHECK_DF | _CRTDBG_CHECK_ALWAYS_DF;
-    _CrtSetDbgFlag(flags);
-  }
-#endif  // _DEBUG
-
-  SetDllDirectory(_T(""));
-  SetSearchPathMode(BASE_SEARCH_PATH_ENABLE_SAFE_SEARCHMODE |
-                    BASE_SEARCH_PATH_PERMANENT);
-
+int __stdcall wWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/,
+                       wchar_t* /*command_line*/, int /*show_mode*/) {
   HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, nullptr, 0);
 
-  base::AtExitManager atexit_manager;
-  base::CommandLine::Init(0, nullptr);
+  SetSearchPathMode(BASE_SEARCH_PATH_ENABLE_SAFE_SEARCHMODE |
+                    BASE_SEARCH_PATH_PERMANENT);
+  SetDllDirectory(_T(""));
+
+#ifdef _DEBUG
+    int debug_flags = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+    debug_flags |= _CRTDBG_LEAK_CHECK_DF | _CRTDBG_CHECK_ALWAYS_DF;
+    _CrtSetDbgFlag(debug_flags);
+#endif  // _DEBUG
+
+  base::AtExitManager at_exit_manager;
+
+  if (base::CommandLine::Init(0, nullptr)) {
+    base::AtExitManager::RegisterCallback([](void* /*param*/) {
+      base::CommandLine::Reset();
+    }, nullptr);
+  } else {
+    TaskDialog(NULL, hInstance, MAKEINTRESOURCE(IDR_MAIN_FRAME),
+               nullptr, MAKEINTRESOURCE(IDS_ERR_INIT_FAILED), TDCBF_OK_BUTTON,
+               TD_ERROR_ICON, nullptr);
+    return __LINE__;
+  }
 
   logging::LoggingSettings logging_settings;
   logging_settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
-  logging::InitLogging(logging_settings);
+  if (!logging::InitLogging(logging_settings)) {
+    TaskDialog(NULL, hInstance, MAKEINTRESOURCE(IDR_MAIN_FRAME),
+               nullptr, MAKEINTRESOURCE(IDS_ERR_INIT_FAILED), TDCBF_OK_BUTTON,
+               TD_ERROR_ICON, nullptr);
+    return __LINE__;
+  }
 
   logging::LogEventProvider::Initialize(GUID_JUNO_APPLICATION);
+  base::AtExitManager::RegisterCallback([](void* /*param*/) {
+    logging::LogEventProvider::Uninitialize();
+  }, nullptr);
 
-  CString message;
-  message.LoadString(IDS_ERR_INIT_FAILED);
-
-  HRESULT result = S_OK;
-
-  result = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
-  ATLASSERT(SUCCEEDED(result));
-  if (FAILED(result)) {
+  HRESULT result = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+  if (SUCCEEDED(result)) {
+    base::AtExitManager::RegisterCallback([](void* /*param*/) {
+      CoUninitialize();
+    }, nullptr);
+  } else {
     LOG(ERROR) << "CoInitializeEx failed: 0x" << std::hex << result;
-    MessageBox(NULL, message, nullptr, MB_ICONERROR);
+    TaskDialog(NULL, hInstance, MAKEINTRESOURCE(IDR_MAIN_FRAME),
+               nullptr, MAKEINTRESOURCE(IDS_ERR_INIT_FAILED), TDCBF_OK_BUTTON,
+               TD_ERROR_ICON, nullptr);
     return __LINE__;
   }
 
-  result = AtlInitCommonControls(0xFFFF);  // all classes
-  ATLASSERT(result != FALSE);
-  if (!result) {
+  if (!AtlInitCommonControls(0xFFFF)) {  // all classes
     LOG(ERROR) << "AtlInitCommonControls failed";
-    MessageBox(NULL, message, nullptr, MB_ICONERROR);
+    TaskDialog(NULL, hInstance, MAKEINTRESOURCE(IDR_MAIN_FRAME),
+               nullptr, MAKEINTRESOURCE(IDS_ERR_INIT_FAILED), TDCBF_OK_BUTTON,
+               TD_ERROR_ICON, nullptr);
     return __LINE__;
   }
 
-  madoka::net::WinSock winsock(WINSOCK_VERSION);
-  ATLASSERT(winsock.Initialized());
-  if (!winsock.Initialized()) {
-    LOG(ERROR) << "WinSock startup failed: " << winsock.error();
-    MessageBox(NULL, message, nullptr, MB_ICONERROR);
+  WSADATA wsa_data;
+  int error = WSAStartup(WINSOCK_VERSION, &wsa_data);
+  if (error == 0) {
+    DLOG(INFO) << wsa_data.szDescription << " is " << wsa_data.szSystemStatus;
+    base::AtExitManager::RegisterCallback([](void* /*param*/) {
+      WSACleanup();
+    }, nullptr);
+  } else {
+    LOG(ERROR) << "WinSock startup failed: " << error;
+    TaskDialog(NULL, hInstance, MAKEINTRESOURCE(IDR_MAIN_FRAME),
+               nullptr, MAKEINTRESOURCE(IDS_ERR_INIT_FAILED), TDCBF_OK_BUTTON,
+               TD_ERROR_ICON, nullptr);
     return __LINE__;
   }
 
   url::Initialize();
+  base::AtExitManager::RegisterCallback([](void* /*param*/) {
+    url::Shutdown();
+  }, nullptr);
 
   result = _Module.Init(nullptr, hInstance);
-  ATLASSERT(SUCCEEDED(result));
-  if (FAILED(result)) {
+  if (SUCCEEDED(result)) {
+    base::AtExitManager::RegisterCallback([](void* /*param*/) {
+      _Module.Term();
+    }, nullptr);
+  } else {
     LOG(ERROR) << "CAppModule::Init failed: 0x" << std::hex << result;
-    MessageBox(NULL, message, nullptr, MB_ICONERROR);
+    TaskDialog(NULL, hInstance, MAKEINTRESOURCE(IDR_MAIN_FRAME),
+               nullptr, MAKEINTRESOURCE(IDS_ERR_INIT_FAILED), TDCBF_OK_BUTTON,
+               TD_ERROR_ICON, nullptr);
     return __LINE__;
   }
 
-  {
-    CMessageLoop message_loop;
-    _Module.AddMessageLoop(&message_loop);
-
-    MainFrame frame;
-    if (frame.CreateEx())
-      message_loop.Run();
-
-    _Module.RemoveMessageLoop();
+  CMessageLoop message_loop;
+  if (!_Module.AddMessageLoop(&message_loop)) {
+    LOG(ERROR) << "Failed to create message loop.";
+    TaskDialog(NULL, hInstance, MAKEINTRESOURCE(IDR_MAIN_FRAME),
+               nullptr, MAKEINTRESOURCE(IDS_ERR_INIT_FAILED), TDCBF_OK_BUTTON,
+               TD_ERROR_ICON, nullptr);
+    return __LINE__;
   }
 
-  _Module.Term();
-  url::Shutdown();
-  CoUninitialize();
-  logging::LogEventProvider::Uninitialize();
-  base::CommandLine::Reset();
+  MainFrame frame;
+  frame.CreateEx();
+
+  message_loop.Run();
+
+  if (!_Module.RemoveMessageLoop())
+    LOG(WARNING) << "Failed to remove message loop.";
 
   return 0;
 }
